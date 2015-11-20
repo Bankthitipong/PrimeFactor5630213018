3b7962a6e840bda4597975f6c9aac669438c4d57
bank <titipong_bb@hotmail.com>
2015-11-20 13:25:01 +0700

    CREATED file HW_PrimeFactor.java

---
+++ HW_PrimeFactor.java
@@ -0,0 +1,37 @@
+package hw_primefactor;
+
+import java.util.Scanner; // Thitipong Ngansathin SE 5630213018
+
+/**
+ *
+ * @author Kyo
+ */
+public class HW_PrimeFactor {
+
+    public static void main(String[] args) {
+
+        int inputnumber, primenumber;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("You want Exit ,Please Enter 0 \n");
+        do {
+            System.out.print("Enter number : ");
+            inputnumber = in.nextInt();
+
+            System.out.print("Prime result : ");
+            for (int i = 2; i <= inputnumber; i++) {
+                while (inputnumber % i == 0) {
+                    primenumber = i;
+                    inputnumber = inputnumber / primenumber;
+                    System.out.print(primenumber);
+                    if (inputnumber >= primenumber) {
+                        System.out.print(" x ");
+                    }
+                }
+            }
+            System.out.print("\n\n");
+        } while (inputnumber != 0);
+
+    }
+
+}
